33ec63a4d302ddd884e5db9a3fd234c3662643f1
Coralie <coralie.burtin@protonmail.com>
2024-03-08 09:29:59 +0100

    handle errors

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -3,10 +3,11 @@
 namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response};
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
@@ -15,20 +16,18 @@ class ExceptionSubscriber implements EventSubscriberInterface
         $exception = $event->getThrowable();
 
         if ($exception instanceof HttpException) {
-            $data = [
-                'status' => $exception->getStatusCode(),
-                'message' => $exception->getMessage()
-            ];
-
-            $event->setResponse(new JsonResponse($data));
+            $status = $exception->getStatusCode();
+        } else if ($exception instanceof NotEncodableValueException) {
+            $status = Response::HTTP_BAD_REQUEST;
         } else {
-            $data = [
-                'status' => 500,
-                'message' => $exception->getMessage(),
-            ];
-
-            $event->setResponse(new JsonResponse($data));
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
         }
+
+        $data = [
+            'status' => $status,
+            'message' => $exception->getMessage(),
+        ];
+        $event->setResponse(new JsonResponse($data, $status));
     }
 
     public static function getSubscribedEvents(): array
